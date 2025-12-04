4cf7d183d7463f7cdec64b1b7f5a97f6c4716edf
Sergio Ortega Culebra <sortcul079@g.educaand.es>
2025-12-04 17:32:54 +0100

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -6,7 +6,9 @@
 */
 public class HolaMundo {
 	public static void main(String[] args){
-	System.out.println("Hola mundo");
-	System.out.printls("Aquí estamos haciendo pruebas git");
+	String naranja="\033[33m";
+	String azul="\033[34m";
+	System.out.println(naranja+"Hola mundo");
+	System.out.println(azul+"Aquí estamos haciendo pruebas git");
 	}
 }
